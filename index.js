b5c440b7a42031dd4a1355150d3570747565c265
sluongo718 <sluongo718@yahoo.com>
2021-07-08 00:12:09 -0400

    commit tree 826f7e86ce45149179512068e064eccb64b55715

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+  const map =  (arr, func) => {
+      return arr.map(n => func(n))
+  }
+
+  const reduce = (arr, func, startingPoint = 0) => {
+    if (!startingPoint) {
+        return arr.reduce((e, memo) => func(e, memo))
+    } else {
+        return arr.reduce((e, memo) => func(e, memo),startingPoint)
+    }
+  }
